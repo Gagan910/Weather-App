39021c3ac2a4d1756113644ce0f3fb835e87f9e1
Gagan Sharma <141417147+Gagan910@users.noreply.github.com>
2024-06-25 22:03:44 +0530

    Update Dist_Code.py

--- Dist_Code.py
+++ Dist_Code.py
@@ -9,23 +9,22 @@ def get_weather(city):
     url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
     response = requests.get(url)
     if response.status_code == 200:
-        weather_data = json.loads(response.text)
-        temperature = weather_data["current"]["temp_c"]
-        return temperature
+        data = response.json()
+        return data["current"]["temp_c"]
     else:
-        st.error("Failed to fetch weather data")
+        st.error("Error fetching data from the API")
         return None
 
 def speak_weather(city, temperature):
     tts = gTTS(text=f'The weather in {city} is {temperature} degree Celsius', lang='en')
     tts.save("weather.mp3")
-    os.system("mpg321 weather.mp3")
+    audio_file = open("weather.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format="audio/mp3")
 
-st.title("Weather Speaker")
-st.write("Welcome to RoboSpeaker 2.1. Created by Gagan Sharma")
+st.title("Weather App")
 
 city = st.text_input("Enter city name:")
-
 if st.button("Get Weather"):
     if city:
         temperature = get_weather(city)
